05b54e542630900512b6fbeeac6454c813846682
miguel <miguel-gutierrez@hotmail.de>
2021-12-06 08:34:11 +0100

    Initial commit

---
+++ src/test/java/com/miguel/engeneering/frozzenlist/FrozzenListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.engeneering.frozzenlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrozzenListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
